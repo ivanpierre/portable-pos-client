35a6a65dca9d192be520b7bfcae03c6ea3e6245c
ivanpierre <kilroysoft@gmail.com>
2018-10-01 08:59:37 +0200

    pass to client

--- src-clj/pwa_clojure/main.clj
+++
@@ -1,9 +0,0 @@
-(ns pwa-clojure.main
-  (:gen-class)
-  (:require [ring.adapter.jetty :as jetty]
-            [pwa-clojure.app :as app]))
-
-(def http-port 3000)
-
-(defn -main []
-  (jetty/run-jetty app/app {:port http-port}))
